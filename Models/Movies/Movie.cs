39647419a1cf1fecbe28d1d79517177603b0d77a
Azamat Erbosyn <Erbosyn75@gmail.com>
2020-04-11 11:53:25 +0600

    Initial commit

---
+++ Models/Movies/Movie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace myAsp.Models.Movies
+{
+
+    public class Movie
+    {
+        /// <summary>
+        /// Movie id
+        /// </summary>
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Movie poster 
+        /// </summary>
+        [Display(Name = "Url to poster")]
+        public string Poster { get; set; }
+
+        /// <summary>
+        /// Movie name
+        /// </summary>
+        [Display(Name = "Movie name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Author
+        /// </summary>
+        [Display(Name = "Author name")]
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Movie genere
+        /// </summary>
+        [Display(Name = "List of genres (by comma)")]
+        public string Genre { get; set; }
+
+        /// <summary>
+        /// Creation date
+        /// </summary>
+        [Display(Name = "Release date")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
